015f4d31194827bce334481204206b5dabfe876e
nmateev <bear@linux.local>
2020-03-03 18:58:09 +0200

    Change build gradle pipeline

--- vars/buildGradlePipeline.groovy
+++ vars/buildGradlePipeline.groovy
@@ -1,4 +1,36 @@
 
 def call(){
-    println('Hello world')
+    pipeline {
+        agent { label 'generic' }
+        stages {
+            stage ('Clean workspace') {
+                steps {
+                    cleanWs()
+                    sh 'pwd'
+                    sh 'ls'
+                }
+            }
+
+            stage ('Checkout') {
+                steps {
+                    git 'https://github.com/nmateev/example.git'
+                    sh 'pwd'
+                    sh 'ls'
+                }
+            }
+
+            stage ('Build') {
+                steps {
+                    sh 'chmod +x ./gradlew'
+                    sh './gradlew clean build'
+                }
+            }
+
+            stage ('Archive') {
+                steps {
+                    archiveArtifacts 'build/libs/*.jar'
+                }
+            }
+        }
+    }
 }
